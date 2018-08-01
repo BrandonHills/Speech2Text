b3927c0afc11f318b496c90752ec96b924896ba4
Brandon Hills <brandon_w_hills@college.harvard.edu>
2018-08-01 15:43:17 +0530

    prepare data

--- recognizeWav.py
+++ recognizeWav.py
@@ -2,15 +2,15 @@ import os
 import sys
 
 def main():
-	s2t = RecognizeWav("/home/eundlpoc002adm/brandon-s2t/kaldi/egs/librispeech/s5/")
+	s2t = RecognizeWav("/home/eundlpoc002adm/brandon-s2t/kaldi/egs/librispeech/s5/", "data/recognizeWav/")
 
 	s2t.prepareData(s2t.dirPath +"data/recognizeWav/inputData/input.flac")
 
 class RecognizeWav:
 
-	def __init__(self, dirPath):
+	def __init__(self, dirPath, dataPath):
 		self.dirPath = dirPath
-		self.data = "data/recognizeWav/"
+		self.data = dataPath
 
 
 	def prepareData(self, audioPath):
@@ -18,9 +18,11 @@ class RecognizeWav:
 		print("Creating wav.scp file")
 		# Create wav.scp file
 
-		location = self.dirPath + self.data + "wav.scp"
+		location = self.dirPath + self.data + 'wav.scp'
 		print("OPENING wav.scp file in ", location)
-		with open(self.dirPath + 'wav.scp', 'w+') as the_file:
+		os.system("rm " + location)
+
+		with open(location, 'w+') as the_file:
 			the_file.write('input-0000 flac -c -d -s ' + audioPath + " |")
 
 
