a5340cc1816481955834d13d46ba131f800de723
Brandon Hills <brandon_w_hills@college.harvard.edu>
2018-08-01 14:39:36 +0530

    prepare data

--- recognizeWav.py
+++ recognizeWav.py
@@ -1,7 +1,30 @@
 import os
+import sys
 
 def main():
-	
+	s2t = RecognizeWav("/home/eundlpoc002adm/brandon-s2t/kaldi/egs/librispeech/s5/")
+
+	s2t.prepareData("data/recognizeWav/inputData/input.flac")
+
+class RecognizeWav:
+
+	def __init__(self, dirPath):
+		self.dirPath = dirPath
+		self.data = "data/recognizeWav/"
+
+
+	def prepareData(self, audioPath):
+		move = "mv " + audioPath + " " + self.dirPath + self.data
+		os.system(move)
+
+		# Create wav.scp file
+		with open('wav.scp', 'w+') as the_file:
+    		the_file.write('input-0000 flac -c -d -s ' + self.audioPath)
+
+
+
+
+
 
 
 
